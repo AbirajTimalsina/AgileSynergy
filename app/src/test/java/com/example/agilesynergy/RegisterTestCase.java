6238acdcdcb303853b28a96ff6742b0b5cf73e55
SsumanShahi <shaggysy201118@gmail.com>
2020-06-13 21:59:51 +0545

    First Registration Unit Test successfully done

---
+++ app/src/test/java/com/example/agilesynergy/RegisterTestCase.java
@@ -0,0 +1,22 @@
+package com.example.agilesynergy;
+
+import com.example.agilesynergy.classes.userRegister;
+import com.example.agilesynergy.models.user;
+import static junit.framework.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class RegisterTestCase {
+
+    @Test
+    public void RegisterTest(){
+
+
+        user newuser = new user("DumanShahi","9849532863","suman1234@gmail.com"
+        ,"1234","null",null);
+        userRegister UserRegister = new userRegister(newuser);
+        boolean userRegistration = UserRegister.userRegistration();
+        assertTrue(userRegistration);
+
+    }
+}
