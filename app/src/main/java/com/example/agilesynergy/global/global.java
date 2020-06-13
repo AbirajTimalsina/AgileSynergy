01f0b7ff154115fdb33d8b05f9d1b3aba8dd5bfd
SsumanShahi <shaggysy201118@gmail.com>
2020-06-13 21:59:51 +0545

    First Registration Unit Test successfully done

--- app/src/main/java/com/example/agilesynergy/global/global.java
+++ app/src/main/java/com/example/agilesynergy/global/global.java
@@ -1,19 +1,17 @@
 package com.example.agilesynergy.global;
 
-import com.example.agilesynergy.models.item;
 import com.example.agilesynergy.models.user;
 
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class global {
-    public static final String Base_URL = "http://10.0.2.2:3000/";
-    //public static final String Base_URL = "http://192.168.1.71:3000/";//localhost ip address
+    //public static final String Base_URL = "http://10.0.2.2:3000/"; //localhost ip address
+    public static final String Base_URL="http://192.168.0.113:3000/";//fortesting
     public static String token = "Bearer ";
     public static String imagePath = Base_URL + "uploads/" ;
 
     public static user user;
-    public static item item;
 
     public static Retrofit getInstance() {
         Retrofit retrofit = new Retrofit.Builder()
@@ -21,6 +19,5 @@ public class global {
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         return retrofit;
-
     }
 }
