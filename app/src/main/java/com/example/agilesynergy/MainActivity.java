92e2dd5517a2c8faadc4b889702e07a24f0ee8c6
sandesnp <averagenp@gmail.com>
2020-06-16 19:21:06 +0545

    Bottom Tablayout navigation using ViewPager

--- app/src/main/java/com/example/agilesynergy/MainActivity.java
+++ app/src/main/java/com/example/agilesynergy/MainActivity.java
@@ -1,17 +1,48 @@
 package com.example.agilesynergy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.Navigation;
+import androidx.viewpager.widget.ViewPager;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+
+import com.example.agilesynergy.fragments.fragmentProfile;
+import com.google.android.material.tabs.TabLayout;
+
+import com.example.agilesynergy.adapter.ViewPagerAdapter;
+import com.example.agilesynergy.fragments.fragmentDashboard;
+import com.example.agilesynergy.fragments.fragmentMenu;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static ViewPager viewPager;
+
+    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // something is different 2
+        //Fragment Handling
+
+        viewPager = findViewById(R.id.viewPager);
+        tabLayout = findViewById(R.id.tabID);
+
+        tabLayout.setSelectedTabIndicatorColor(Color.parseColor("#FFFFFF"));
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        viewPagerAdapter.addFragment(new fragmentDashboard(), "Dashboard");
+        viewPagerAdapter.addFragment(new fragmentMenu(), "Menu");
+        viewPagerAdapter.addFragment(new fragmentProfile(), "Profile");
+
+        viewPagerAdapter.notifyDataSetChanged();
+        viewPager.setAdapter(viewPagerAdapter);
+
+        tabLayout.setupWithViewPager(viewPager);
 
     }
 }
