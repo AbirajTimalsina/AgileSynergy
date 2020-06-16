ce3b4c5c8631a7c0b0bc048479c4d0330b7ae4fe
sandesnp <averagenp@gmail.com>
2020-06-16 19:21:06 +0545

    Bottom Tablayout navigation using ViewPager

---
+++ app/src/main/java/com/example/agilesynergy/fragments/fragmentDashboard.java
@@ -0,0 +1,25 @@
+package com.example.agilesynergy.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.agilesynergy.R;
+
+
+public class fragmentDashboard extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.fragment_dashboard, container, false);
+
+        return view;
+    }
+}
