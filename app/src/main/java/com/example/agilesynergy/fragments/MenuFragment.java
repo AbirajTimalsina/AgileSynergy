018d2a8b4d8e0368134e521d8d8c83edb5e1f6fb
sandesnp <averagenp@gmail.com>
2020-06-17 23:13:56 +0545

    Bottom Tablayout navigation using ViewPager

--- app/src/main/java/com/example/agilesynergy/fragments/MenuFragment.java
+++ app/src/main/java/com/example/agilesynergy/fragments/MenuFragment.java
@@ -3,64 +3,53 @@ package com.example.agilesynergy.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.agilesynergy.R;
+import com.example.agilesynergy.adapter.RecyclerAdapter;
+import com.example.agilesynergy.api.itemapi;
+import com.example.agilesynergy.global.global;
+import com.example.agilesynergy.models.item;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MenuFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Response;
+
+
 public class MenuFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public MenuFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MenuFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MenuFragment newInstance(String param1, String param2) {
-        MenuFragment fragment = new MenuFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    RecyclerView recyclerView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu, container, false);
+        View view = inflater.inflate(R.layout.fragment_menu, container, false);
+        recyclerView = view.findViewById(R.id.recyclerviewmenu);
+        itemapi itemapi = global.getInstance().create(itemapi.class);
+        Call<List<item>> itemList = itemapi.getAllItem();
+
+        try {
+            Response<List<item>> itemResponse = itemList.execute();
+            if (itemResponse.isSuccessful()) {
+
+                RecyclerAdapter recyclerAdapter = new RecyclerAdapter(getContext(), itemResponse.body());
+                recyclerView.setAdapter(recyclerAdapter);
+                LinearLayoutManager layoutManager
+                        = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+                recyclerView.setLayoutManager(layoutManager);
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return view;
     }
 }
