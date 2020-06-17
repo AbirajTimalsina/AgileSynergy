becb5c923483f954e04e00c0c85f737436aec84e
sandesnp <averagenp@gmail.com>
2020-06-17 23:13:56 +0545

    Bottom Tablayout navigation using ViewPager

---
+++ app/src/main/java/com/example/agilesynergy/api/itemapi.java
@@ -0,0 +1,15 @@
+package com.example.agilesynergy.api;
+
+import com.example.agilesynergy.models.item;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface itemapi {
+
+    @GET("item/all")
+    Call<List<item>> getAllItem();
+
+}
