e0e2ed0718ee149ecdcd12488cab93a0e597469f
Sabin <sabingautam05@gmail.com>
2020-06-20 19:18:48 +0545

    homepage design Version 1.2

--- app/src/main/java/com/example/agilesynergy/fragments/HomeFragment.java
+++ app/src/main/java/com/example/agilesynergy/fragments/HomeFragment.java
@@ -32,6 +32,7 @@ import com.example.agilesynergy.models.upcomingfoodModel;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import retrofit2.Call;
 import retrofit2.Callback;
