855ec4484d821f3b46fcd8aa6e845f28366e593e
Sheelabhan <shilubhandari23@gmail.com>
2020-07-01 23:17:49 +0530

    i have done searching for menu items.

--- app/src/main/java/com/example/agilesynergy/adapter/RecyclerAdapter.java
+++ app/src/main/java/com/example/agilesynergy/adapter/RecyclerAdapter.java
@@ -9,137 +9,132 @@ import android.widget.Filter;
 import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.agilesynergy.R;
-import com.example.agilesynergy.fragments.MenuFragment;
 import com.example.agilesynergy.fragments.innerFragments.ItemFragment;
 import com.example.agilesynergy.global.global;
 import com.example.agilesynergy.models.item;
 import com.squareup.picasso.Picasso;
 
-import org.w3c.dom.Text;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.RecyclerViewHolder> implements Filterable {
-
-    Context mcontext;
-    private List<item> listItems;
-    List<item> itemFilter;
-    FragmentManager fm;
-
-    public RecyclerAdapter(Context mcontext, List<item> listItems, FragmentManager fm) {
-        this.mcontext = mcontext;
-        this.listItems = listItems;
-        this.itemFilter=listItems;
-        this.fm = fm;
-    }
-
-    @NonNull
-    @Override
-    public RecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_items, parent, false);
-        return new RecyclerViewHolder(view);
-    }
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.RecyclerViewHolder>  {
 
 
-    @Override
-    public void onBindViewHolder(@NonNull RecyclerViewHolder holder, int position) {
-
-    System.out.println(position +"");
-
-        String imagePath = global.imagePath + itemFilter.get(position).getItempicture();
-        holder.itemname.setText(itemFilter.get(position) .getItemname());
-        holder.itemprice.setText(itemFilter.get(position).getItemprice());
-        holder.itemingredient.setText(itemFilter.get(position).getItemingredient());
-
-
-        final item item = itemFilter.get(position);
-
-
-        Picasso.get().load(imagePath).into(holder.imageitempicture);
-
-        holder.linearLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                //bundle Arguments is not working.
-                ItemFragment itemFragment = new ItemFragment();
-                Bundle bundle = new Bundle();
-                bundle.putParcelable("itemObject", item);
-                itemFragment.setArguments(bundle);
-                global.item=item;
-
-
-                fm.beginTransaction().replace(R.id.frame_container, new ItemFragment()).addToBackStack(null).commit();
+        private Context mcontext;
+        private List<item> listItems;
+        private ArrayList<JSONObject> listObjects = new ArrayList<>();
+        private FragmentManager fm;
+        private String location_Fragment;
+        public RecyclerAdapter(Context mcontext, List<item> listItems, ArrayList<JSONObject> listObjects,
+                               FragmentManager fm, String location_Fragment) {
+            this.mcontext = mcontext;
+            this.listItems = listItems;
+            this.listObjects = listObjects;
+            this.fm = fm;
+            this.location_Fragment = location_Fragment;
+        }
+        @NonNull
+        @Override
+        public RecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = null;
+            switch (location_Fragment) {
+                case "menu":
+                    view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_items, parent, false);
+                    break;
+                case "checkout":
+                    view = LayoutInflater.from(parent.getContext()).inflate(R.layout.checkout_item, parent, false);
+                    break;
             }
-        });
-    }
+            return new RecyclerViewHolder(view);
+        }
 
-    @Override
-    public int getItemCount() {
-        return itemFilter.size();
-    }
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerViewHolder holder, int position) {
+            switch (location_Fragment) {
+                case "menu":
+                    final item item = listItems.get(position);
+                    holder.itemname.setText(item.getItemname());
+                    holder.itemprice.setText(item.getItemprice());
+                    holder.itemingredient.setText(item.getItemingredient());
+                    String imagePath = global.imagePath + item.getItempicture();
+                    Picasso.get().load(imagePath).into(holder.imageitempicture);
+                    holder.linearLayout.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            //bundle Arguments is not working.
+                            ItemFragment itemFragment = new ItemFragment();
+                            Bundle bundle = new Bundle();
+                            bundle.putParcelable("itemObject", item);
+                            itemFragment.setArguments(bundle);
+                            global.item = item;
 
-    @Override
-    public Filter getFilter() {
-        return new Filter() {
-            @Override
-            protected FilterResults performFiltering(CharSequence constraint) {
-                String Key = constraint.toString();
-                if(Key.isEmpty()){
-                    itemFilter= listItems;
-                }else {
-                    List<item> itemArrayList= new ArrayList<>();
-                    for(item row: listItems){
-                        if(row.getItemname().toLowerCase().contains(Key.toLowerCase())){
-                            itemArrayList.add(row);
+                            fm.beginTransaction().replace(R.id.frame_container, new ItemFragment()).addToBackStack(null).commit();
                         }
-
-                    }
-                    itemFilter=itemArrayList;
+                    });
+                    break;
+                case "checkout":
+                    JSONObject items = listObjects.get(position);
+                    holder.checkoutItemName.setText(items.optString("itemname").toString());
+                    holder.checkoutItemPrice.setText(items.optString("itemprice").toString());
+                    holder.checkoutItemAmount.setText(items.optString("itemamount").toString());
+                    Integer Price, Amount, afterAmount;
+                    Price = Integer.parseInt(items.optString("itemprice").toString());
+                    Amount = Integer.parseInt(items.optString("itemamount").toString());
+                    afterAmount = Price * Amount;
+                    holder.checkoutItemAfterAmount.setText(Integer.toString(afterAmount));
+                    break;
+            }
+        }
+        int listCount = 0;
+        @Override
+        public int getItemCount() {
+            switch (location_Fragment) {
+                case "menu":
+                    listCount = listItems.size();
+                    break;
+                case "checkout":
+                    listCount = listObjects.size();
+                    break;
+            }
+            return listCount;
+        }
+        public class RecyclerViewHolder extends RecyclerView.ViewHolder {
+            //Menu Elements
+            ImageView imageitempicture;
+            TextView itemname, itemprice, itemingredient;
+            LinearLayout linearLayout;
+            //Checkout Elements
+            TextView checkoutItemName, checkoutItemPrice, checkoutItemAmount, checkoutItemAfterAmount;
+            public RecyclerViewHolder(@NonNull View itemView) {
+                super(itemView);
+                switch (location_Fragment) {
+                    case "menu":
+                        imageitempicture = itemView.findViewById(R.id.imageviewmenupicture);
+                        itemname = itemView.findViewById(R.id.itemmenuname);
+                        itemprice = itemView.findViewById(R.id.itemmenuprice);
+                        itemingredient = itemView.findViewById(R.id.itemmenuingredient);
+                        linearLayout = itemView.findViewById(R.id.linearmenu);
+                        break;
+                    case "checkout":
+                        checkoutItemName = itemView.findViewById(R.id.txtcheckoutitemname);
+                        checkoutItemPrice = itemView.findViewById(R.id.txtcheckoutitemprice);
+                        checkoutItemAmount = itemView.findViewById(R.id.txtcheckoutitemamount);
+                        checkoutItemAfterAmount = itemView.findViewById(R.id.txtcheckoutitemafteramount);
+                        break;
                 }
 
-                FilterResults filterResults= new FilterResults();
-                filterResults.values= itemFilter;
-                return filterResults;
-
-
             }
 
-            @Override
-            protected void publishResults(CharSequence constraint, FilterResults results) {
-               itemFilter= (List<item>) results.values;
-                notifyDataSetChanged();
             }
-        };
-    }
-
-
-    public class RecyclerViewHolder extends RecyclerView.ViewHolder {
-
-        ImageView imageitempicture;
-        TextView itemname, itemprice, itemingredient;
-        LinearLayout linearLayout;
-
-        public RecyclerViewHolder(@NonNull View itemView) {
-            super(itemView);
-
-            imageitempicture = itemView.findViewById(R.id.imageviewmenupicture);
-            itemname = itemView.findViewById(R.id.itemmenuname);
-            itemprice = itemView.findViewById(R.id.itemmenuprice);
-            itemingredient = itemView.findViewById(R.id.itemmenuingredient);
-            linearLayout = itemView.findViewById(R.id.linearmenu);
-
         }
-    }
-}
+
+
--- app/src/main/java/com/example/agilesynergy/fragments/MenuFragment.java
+++ app/src/main/java/com/example/agilesynergy/fragments/MenuFragment.java
@@ -12,7 +12,12 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.agilesynergy.R;
 import com.example.agilesynergy.adapter.RecyclerAdapter;
@@ -20,52 +25,47 @@ import com.example.agilesynergy.api.itemapi;
 import com.example.agilesynergy.global.global;
 import com.example.agilesynergy.models.item;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
+import retrofit2.Callback;
 import retrofit2.Response;
+import retrofit2.Retrofit;
 
 
 public class MenuFragment extends Fragment {
 
-AutoCompleteTextView autocompletetextmenusearch;
-    RecyclerView recyclerView;
-RecyclerAdapter recyclerAdapter;
+
+    AutoCompleteTextView autocompletetextmenusearch;
+    RecyclerView recyclerView, searchrecycle;
+
+    TextView etName;
+    private Map<String, String> MenuItems;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_menu, container, false);
-  autocompletetextmenusearch=view.findViewById(R.id.autocompletetextmenusearch);
+
+        autocompletetextmenusearch = view.findViewById(R.id.autocompletetextmenusearch);
+        etName = view.findViewById(R.id.etName);
+        searchrecycle=view.findViewById(R.id.searchrecycle);
+        MenuItems = new HashMap<>();
         recyclerView = view.findViewById(R.id.recyclerviewmenu);
         itemapi itemapi = global.getInstance().create(itemapi.class);
         Call<List<item>> itemList = itemapi.getAllItem();
-
-  autocompletetextmenusearch.addTextChangedListener(new TextWatcher() {
-      @Override
-      public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-      }
-
-      @Override
-      public void onTextChanged(CharSequence s, int start, int before, int count) {
-          recyclerAdapter.getFilter().filter(s);
-      }
-
-      @Override
-      public void afterTextChanged(Editable s) {
-
-      }
-  });
-
-
         try {
             Response<List<item>> itemResponse = itemList.execute();
             if (itemResponse.isSuccessful()) {
+                global.itemList = itemResponse.body();
 
                 FragmentManager fm = getActivity().getSupportFragmentManager();
 
-                RecyclerAdapter recyclerAdapter = new RecyclerAdapter(getContext(), itemResponse.body(),null, fm, "menu");
+                RecyclerAdapter recyclerAdapter = new RecyclerAdapter(getContext(), itemResponse.body(), null, fm, "menu");
                 recyclerView.setAdapter(recyclerAdapter);
                 LinearLayoutManager layoutManager
                         = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
@@ -75,6 +75,41 @@ RecyclerAdapter recyclerAdapter;
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        //Selecting one game then comparing it to all games till it matches. Data is retrieved from matched data
+        for (item Item : global.itemList) {
+            MenuItems.put(Item.getItemname(), Item.get_id()); //(key, value)
+        }
+
+
+        ArrayAdapter arrayAdapterCountry = new ArrayAdapter<>(getActivity(),
+                android.R.layout.select_dialog_item, new ArrayList(MenuItems.keySet()));
+        autocompletetextmenusearch.setAdapter(arrayAdapterCountry);
+        autocompletetextmenusearch.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String key = adapterView.getItemAtPosition(i).toString(); //extracting _id with respect with selected game
+                String ItemID = MenuItems.get(key);  //getting value through the key
+                List<item> SearchItemlist = new ArrayList<>();
+                for (item item : global.itemList) {
+                    if (ItemID.equals(item.get_id())) {
+                        SearchItemlist.add(item);
+                    }
+                }
+                FragmentManager fm = getActivity().getSupportFragmentManager();
+                RecyclerAdapter recyclerAdapter = new RecyclerAdapter(getContext(), SearchItemlist,
+                        null, fm, "menu");
+                searchrecycle.setAdapter(recyclerAdapter);
+                LinearLayoutManager layoutManager
+                        = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+               searchrecycle.setLayoutManager(layoutManager);
+            }
+        });
+
+
+
         return view;
+
+
     }
 }
