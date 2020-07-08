27fd2378205e2a4357b7575b8a1ce5871d52a63d
sandesnp <averagenp@gmail.com>
2020-07-09 01:59:59 +0545

    Broadcast Progress

--- app/src/main/java/com/example/agilesynergy/SplashScreenActivity.java
+++ app/src/main/java/com/example/agilesynergy/SplashScreenActivity.java
@@ -1,26 +1,55 @@
 package com.example.agilesynergy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.example.agilesynergy.classes.broadcastClass;
+
 public class SplashScreenActivity extends AppCompatActivity {
+
+
+    broadcastClass broadcastClass = new broadcastClass();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        Handler handler = new Handler();
 
+        Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
+
                 Intent intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
             }
-        },3000);
+        }, 3000);
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter intentFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(broadcastClass, intentFilter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(broadcastClass);
     }
 }
---
+++ app/src/main/java/com/example/agilesynergy/classes/broadcastClass.java
@@ -0,0 +1,45 @@
+package com.example.agilesynergy.classes;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.example.agilesynergy.InvalidActivity;
+
+public class broadcastClass extends BroadcastReceiver {
+
+
+    private boolean isConnected;
+
+    public boolean isIsConnected() {
+        return isConnected;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        WifiManager wifi = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);//memory leakage without getApplicationContext
+
+        if (wifi.isWifiEnabled()) {
+            WifiInfo wifiInfo = wifi.getConnectionInfo();
+            if (wifiInfo.getNetworkId() == -1) {
+               isConnected= false;
+            }
+            String myName = wifiInfo.getSSID();
+            Toast.makeText(context, myName, Toast.LENGTH_LONG).show();
+            isConnected= true;
+        }
+        isConnected= false;
+    }
+}
