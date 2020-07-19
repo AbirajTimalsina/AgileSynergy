cf57c5169d8d14be395172877a97a423153c32e4
Sabin <sabingautam05@gmail.com>
2020-07-19 12:26:46 +0545

    completed 3rd sprint

--- app/src/main/java/com/example/agilesynergy/fragments/FavouriteFragment.java
+++ app/src/main/java/com/example/agilesynergy/fragments/FavouriteFragment.java
@@ -1,22 +1,66 @@
 package com.example.agilesynergy.fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.agilesynergy.R;
+import com.example.agilesynergy.adapter.favouriteAdapter;
+import com.example.agilesynergy.api.userapi;
+import com.example.agilesynergy.global.global;
+import com.example.agilesynergy.models.feedbackModel;
+import com.example.agilesynergy.models.user;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class FavouriteFragment extends Fragment {
 
+    RecyclerView fav_recycleview;
+
+    user User;
+    List<feedbackModel> feedbackModels;
+    favouriteAdapter favouriteAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favourite, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_favourite, container, false);
+
+        fav_recycleview = view.findViewById(R.id.fav_recycleview);
+
+        userapi Userapi = global.getInstance().create(userapi.class);
+        Call<user> userCall = Userapi.getUserDetails(global.token);
+
+        userCall.enqueue(new Callback<user>() {
+            @Override
+            public void onResponse(Call<user> call, Response<user> response) {
+                if (!response.isSuccessful()) {
+                    Toast.makeText(getContext(), "Code " + response.code(), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                User = response.body();
+                feedbackModels = response.body().getFeedback();
+                favouriteAdapter = new favouriteAdapter(getContext(), feedbackModels);
+                fav_recycleview.setAdapter(favouriteAdapter);
+                fav_recycleview.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+            }
+
+            @Override
+            public void onFailure(Call<user> call, Throwable t) {
+                Toast.makeText(getContext(), "Error " + t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        return view;
     }
 }
