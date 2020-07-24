9c10db6dc7ede49eb56d60d5c71d9ae0d7f7d147
Apsara <apsaratamang1234@gmail.com>
2020-07-24 11:35:48 +0545

    purchasetesting

--- app/src/androidTest/java/com/example/agilesynergy/LoginTest.java
+++ app/src/androidTest/java/com/example/agilesynergy/LoginTest.java
@@ -1,6 +1,8 @@
 package com.example.agilesynergy;
 
+import androidx.test.espresso.Espresso;
 import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.assertion.ViewAssertions;
 import androidx.test.rule.ActivityTestRule;
 
 import org.junit.Rule;
@@ -9,23 +11,22 @@ import org.junit.Test;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 public class LoginTest {
+
     @Rule
-    public ActivityTestRule<LoginActivity> testRule =
-            new ActivityTestRule<>(LoginActivity.class);
-    String expected = "true";
+    public ActivityTestRule<LoginActivity> testRule = new ActivityTestRule<>(LoginActivity.class);
+
     @Test
-    public void Login() {
-        onView(withId(R.id.etnumber))
-                .perform(typeText("9875149998"))
-                .perform(ViewActions.closeSoftKeyboard());
-        onView(withId(R.id.etPassword))
-                .perform(typeText("1234"))
-                .perform(ViewActions.closeSoftKeyboard());
-        onView(withId(R.id.btnuserlogin))
-                .perform(click());
+    public void checkLogin(){
+        onView(withId(R.id.etnumber)).perform(typeText("9875149998"));
+        Espresso.closeSoftKeyboard();
+        onView(withId(R.id.etPassword)).perform(typeText("1234"));
+        Espresso.closeSoftKeyboard();
+        onView(withId(R.id.btnuserlogin)).perform(click());
+        onView(withId(R.id.const_layout)).check(ViewAssertions.matches(isDisplayed()));
 
     }
 
